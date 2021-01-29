f9f993926fa95bb312d907d06d752cf0d1ac1a39
Alex <soames.alex@gmail.com>
2021-01-28 22:53:16 -0800

    Step Up

---
+++ a1.cpp
@@ -0,0 +1,49 @@
+// a1.cpp
+
+/////////////////////////////////////////////////////////////////////////
+//
+// Student Info
+// ------------
+//
+// Name : <put your full name here!>
+// St.# : <put your full SFU student number here>
+// Email: <put your SFU email address here>
+//
+//
+// Statement of Originality
+// ------------------------
+//
+// All the code and comments below are my own original work. For any non-
+// original work, I have provided citations in the comments with enough detail
+// so that someone can see the exact source and extent of the borrowed work.
+//
+// In addition, I have not shared this work with anyone else, and I have not
+// seen solutions from other students, tutors, websites, books, etc.
+//
+/////////////////////////////////////////////////////////////////////////
+
+#include "a1.h"
+#include "cmpt_error.h"
+#include <iostream>      // no other #includes are allowed
+#include <fstream>       // for this assignment
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+//
+// ... write your functions here ...
+//
+
+//
+// ... put your test functions here (one test function for each 
+//     function above) ... 
+// 
+
+int main() {
+  cout << "Assignment 1!\n";
+
+  //
+  // ... call your test functions here ...
+  //
+}
